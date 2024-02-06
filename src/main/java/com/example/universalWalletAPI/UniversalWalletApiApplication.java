0fab7b78c59ff2b3f7c3f1099314733a6f579901
ishanjain <isjain35@gmail.com>
2024-02-07 00:10:52 +0530

    Initial setup

---
+++ src/main/java/com/example/universalWalletAPI/UniversalWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.universalWalletAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/universalWalletAPI/UniversalWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universalWalletAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
